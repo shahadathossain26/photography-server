80a591d92f3b1bb9dcefa828fdc5519ff11190dd
shahadathossain26 <shiimran172372@gmail.com>
2022-11-09 00:06:01 +0600

    All services data loaded

--- index.js
+++ index.js
@@ -9,17 +9,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.PHOTOGRAPHY_DB_USER)
-console.log(process.env.PHOTOGRAPHY_DB_PASSWORD)
+// console.log(process.env.PHOTOGRAPHY_DB_USER)
+// console.log(process.env.PHOTOGRAPHY_DB_PASSWORD)
 
 const uri = `mongodb+srv://${process.env.PHOTOGRAPHY_DB_USER}:${process.env.PHOTOGRAPHY_DB_PASSWORD}@cluster0.g42knj4.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const servicesCollection = client.db('photography').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
 
 
 app.get('/', (req, res) => {
